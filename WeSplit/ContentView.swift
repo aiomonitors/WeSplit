67dcc5342ff2b1194e9253e479a3cb8ad6b75b53
aiomonitors <sachow168@gmail.com>
2023-04-10 01:33:38 -0400

    Create TextField for Check Amount

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let currencyCode = Locale.current.currency?.identifier ?? "USD"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: currencyCode))
+            }
+            
+            Section {
+                Text(checkAmount, format: .currency(code: currencyCode))
+            }
         }
-        .padding()
     }
 }
 
