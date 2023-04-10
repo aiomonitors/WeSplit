c62072a8f0f2d543c7b5e62db65f5b865ea44743
aiomonitors <sachow168@gmail.com>
2023-04-10 01:54:49 -0400

    Add more form items

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -12,17 +12,40 @@ struct ContentView: View {
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
     
+    let tipPercentages = [16, 18, 20, 22, 24]
+    
     let currencyCode = Locale.current.currency?.identifier ?? "USD"
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Amount", value: $checkAmount, format: .currency(code: currencyCode))
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: currencyCode))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section {
+                    Text(checkAmount, format: .currency(code: currencyCode))
+                }
             }
-            
-            Section {
-                Text(checkAmount, format: .currency(code: currencyCode))
-            }
+            .navigationTitle("WeSplit")
         }
     }
 }
